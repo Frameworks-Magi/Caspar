bfc14832ccd3ab5d66844d7cf8e058a1dff278ea
marcus <marcus@gmail.com>
2025-01-09 19:59:04 +0900

    gen key

--- Api.cs
+++ Api.cs
@@ -75,6 +75,33 @@ namespace Framework.Caspar
             await session.TCS.Task;
         }
 
+        public static void GenerateRSAKey()
+        {
+            using (var rsa = RSA.Create(2048))
+            {
+                // PKCS#1 형식으로 개인키 내보내기
+                var privateKeyBytes = rsa.ExportRSAPrivateKey();
+                var privateKeyPem = new StringBuilder();
+                privateKeyPem.AppendLine("-----BEGIN RSA PRIVATE KEY-----");
+                privateKeyPem.AppendLine(Convert.ToBase64String(privateKeyBytes, Base64FormattingOptions.InsertLineBreaks));
+                privateKeyPem.AppendLine("-----END RSA PRIVATE KEY-----");
+
+                // 공개키 내보내기
+                var publicKeyBytes = rsa.ExportRSAPublicKey();
+                var publicKeyPem = new StringBuilder();
+                publicKeyPem.AppendLine("-----BEGIN RSA PUBLIC KEY-----");
+                publicKeyPem.AppendLine(Convert.ToBase64String(publicKeyBytes, Base64FormattingOptions.InsertLineBreaks));
+                publicKeyPem.AppendLine("-----END RSA PUBLIC KEY-----");
+
+                File.WriteAllText("private_key.pem", privateKeyPem.ToString());
+                File.WriteAllText("public_key.pem", publicKeyPem.ToString());
+            }
+
+            // openssl rsa -pubout -in private_key.pem -out public_key.pem
+            // openssl rsa -in private_key.pem -pubout -out public_key.pem
+            // openssl pkcs8 -topk8 -inform PEM -outform PEM -in public_key.pem -out public_key_pkcs8.pem -nocrypt
+        }
+
         internal static void StartUpDatabase()
         {
 
@@ -2565,26 +2592,6 @@ namespace Framework.Caspar
         {
             try
             {
-
-                // using (var rsa = RSA.Create(2048))
-                // {
-                //     // PKCS#1 형식으로 개인키 내보내기
-                //     var privateKeyBytes = rsa.ExportRSAPrivateKey();
-                //     var privateKeyPem = new StringBuilder();
-                //     privateKeyPem.AppendLine("-----BEGIN RSA PRIVATE KEY-----");
-                //     privateKeyPem.AppendLine(Convert.ToBase64String(privateKeyBytes, Base64FormattingOptions.InsertLineBreaks));
-                //     privateKeyPem.AppendLine("-----END RSA PRIVATE KEY-----");
-
-                //     // 공개키 내보내기
-                //     var publicKeyBytes = rsa.ExportRSAPublicKey();
-                //     var publicKeyPem = new StringBuilder();
-                //     publicKeyPem.AppendLine("-----BEGIN RSA PUBLIC KEY-----");
-                //     publicKeyPem.AppendLine(Convert.ToBase64String(publicKeyBytes, Base64FormattingOptions.InsertLineBreaks));
-                //     publicKeyPem.AppendLine("-----END RSA PUBLIC KEY-----");
-
-                //     File.WriteAllText("private.pem", privateKeyPem.ToString());
-                //     File.WriteAllText("public.pem", publicKeyPem.ToString());
-                // }
 
                 string pem = (string)Config.AWS.CloudFront.PEM;
                 var bytes = Encoding.UTF8.GetBytes(pem);
